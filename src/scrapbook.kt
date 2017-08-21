5290df5473c7e8b185fdb912d85934cdce83e127
Fabio Mascarenhas <mascarenhas@gmail.com>
2017-08-21 11:52:57 -0300

    aula de 21/08: mais ambiguidade

--- src/scrapbook.kt
+++ src/scrapbook.kt
@@ -41,4 +41,67 @@ object scrapbook {
         print(g.derivation("S", 0, 2, 4, 1, 1, 4, 4, 3, 1, 4, 4))
         g.treeDerive("S", 0, 2, 4, 1, 1, 4, 4, 3, 1, 4, 4).printDot("exp_na_aula_1.dot")
     }
+
+    @Test
+    fun test4() {
+        val g = parse.Grammar()
+        g.rules("S", "C");    // 0
+        g.rules("C", "if exp then C") // 1
+        g.rules("C", "if exp then C else C") // 2
+        g.rules("C", "outros") // 3
+        println(g.leftDerive("S", 0, 1, 2, 3, 3))
+        g.treeDerive("S", 0, 1, 2, 3, 3).printDot("dangif_aula_1.dot")
+        println(g.leftDerive("S", 0, 2, 1, 3, 3))
+        g.treeDerive("S", 0, 2, 1, 3, 3).printDot("dangif_aula_2.dot")
+    }
+
+    @Test
+    fun test5() {
+        val g = parse.Grammar()
+        g.rules("S", "E");    // 0
+        g.rules("E", "E + T") // 1
+        g.rules("E", "T")     // 2
+        g.rules("T", "T * F") // 3
+        g.rules("T", "F")     // 4
+        g.rules("F", "( E )") // 5
+        g.rules("F", "num")   // 6
+        println(g.leftDerive("S", 0, 1, 2, 4, 6,
+                3, 4, 6, 6))
+        g.treeDerive("S", 0, 1, 2, 4, 6,
+                3, 4, 6, 6).printDot("expnaoamb_1.dot")
+        println(g.leftDerive("S", 0, 1, 1, 2, 4, 6, 4, 6, 4, 6))
+        g.treeDerive("S", 0, 1, 1, 2, 4, 6, 4, 6, 4, 6).printDot("expnaoamb_2.dot")
+    }
+
+    @Test
+    fun test6() {
+        val g = parse.Grammar()
+        g.rules("S", "E");    // 0
+        g.rules("E", "T + E") // 1
+        g.rules("E", "T")     // 2
+        g.rules("T", "T * F") // 3
+        g.rules("T", "F")     // 4
+        g.rules("F", "( E )") // 5
+        g.rules("F", "num")   // 6
+        println(g.leftDerive("S", 0, 1, 4, 6, 2,
+                3, 4, 6, 6))
+        g.treeDerive("S", 0, 1, 4, 6, 2,
+                3, 4, 6, 6).printDot("expnaoamb_3.dot")
+        println(g.leftDerive("S", 0, 1, 4, 6, 1, 4, 6, 2, 4, 6))
+        g.treeDerive("S", 0, 1, 4, 6, 1, 4, 6, 2, 4, 6).printDot("expnaoamb_4.dot")
+    }
+
+    @Test
+    fun test7() {
+        val g = parse.Grammar()
+        g.rules("S", "C");    // 0
+        g.rules("C", "if exp then C") // 1
+        g.rules("C", "if exp then CE else C") // 2
+        g.rules("C", "outros") // 3
+        g.rules("CE", "if exp then CE else CE") // 4
+        g.rules("CE", "outros") // 5
+        println(g.leftDerive("S", 0, 1, 2, 5, 3))
+        g.treeDerive("S", 0, 1, 2, 5, 3).printDot("dangif_aula_semamb.dot")
+    }
+
 }
