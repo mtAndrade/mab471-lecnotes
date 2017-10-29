2cee8ac7461118c5dfcd21486f31089907a69fac
Giancarllo Rojas <defensedelesprit@gmail.com>
2017-10-29 17:38:29 -0200

    Pequenas modificacoes no jflex e jflex compilado

--- testscanner.kt
+++ testscanner.kt
@@ -4,7 +4,7 @@ object ScannerTest {
     @Test
     fun test1() {
         val f = java.io.FileReader("factorial.java")
-        val scan = Scanner(f)
+        val scan = ScannerMascarenhas(f)
         for (tok in scan.tokens())
             System.out.println(tok)
     }
@@ -12,7 +12,7 @@ object ScannerTest {
     @Test
     fun test2() {
         val f = java.io.FileReader("binarysearch.java")
-        val scan = Scanner(f)
+        val scan = ScannerMascarenhas(f)
         for (tok in scan.tokens())
             System.out.println(tok)
     }
@@ -20,7 +20,7 @@ object ScannerTest {
     @Test
     fun test3() {
         val f = java.io.FileReader("bubblesort.java")
-        val scan = Scanner(f)
+        val scan = ScannerMascarenhas(f)
         for (tok in scan.tokens())
             System.out.println(tok)
     }
@@ -28,7 +28,7 @@ object ScannerTest {
     @Test
     fun test4() {
         val f = java.io.FileReader("treevisitor.java")
-        val scan = Scanner(f)
+        val scan = ScannerMascarenhas(f)
         for (tok in scan.tokens())
             System.out.println(tok)
     }
